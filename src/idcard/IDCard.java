3c9af455bd37da5b06ba51c46253af6e3f03e5b3
splguyjr <splguyjr@naver.com>
2024-10-13 17:34:49 +0900

    Factory Method 패턴

---
+++ src/idcard/IDCard.java
@@ -0,0 +1,27 @@
+package idcard;
+
+import framework.Product;
+
+public class IDCard extends Product {
+    private String owner;
+
+    //package-private 방식으로 같은 패키지 내에서만 접근 가능(subclass 관계여도 사용 불가능)
+    IDCard(String owner) {
+        System.out.println(owner + "의 카드를 만듭니다.");
+        this.owner = owner;
+    }
+
+    @Override
+    public void use() {
+        System.out.println(this + "을 사용합니다.");
+    }
+
+    @Override
+    public String toString() {
+        return "[IDCard:" + owner + "]";
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+}
