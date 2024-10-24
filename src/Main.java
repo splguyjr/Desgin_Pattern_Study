25782a2a56ff88a8f45fa7f4a9c229c0f3d02cdc
splguyjr <splguyjr@naver.com>
2024-10-24 23:28:51 +0900

    Builder 패턴
    예제상에선 Builder(abstract class, Interface), Director(DI를 통해 구체 클래스 주입 받음), Builder 구체 클래스로 구성됨.
    Main Method에서는 구체 클래스와 Director 클래스만을 이용

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
-
+import builder.Director;
+import builder.HTMLBuilder;
+import builder.TextBuilder;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            usage();
+            System.exit(0);
+        }
 
+        if (args[0].equals("text")) {
+            TextBuilder textBuilder = new TextBuilder();
+            Director director = new Director(textBuilder);
+            director.construct();
+            String result = textBuilder.getTextResult();
+            System.out.println(result);
+        }
+
+        else if (args[0].equals("html")) {
+            HTMLBuilder htmlbuilder = new HTMLBuilder();
+            Director director = new Director(htmlbuilder);
+            director.construct();
+            String filename = htmlbuilder.getHTMLResult();
+            System.out.println("HTML파일 " + filename + "이 작성되었습니다.");
+        }
+
+        else {
+            usage();
+            System.exit(0);
+        }
+
+
+    }
+
+    public static void usage() {
+        System.out.println("Usage: java Main text   텍스트로 문서 작성");
+        System.out.println("Usage: java Main html   HTML 파일로 문서 작성");
     }
 }
