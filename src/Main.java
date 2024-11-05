03257cb38e83ecd433bf31cf9ff4b99317562df9
splguyjr <splguyjr@naver.com>
2024-11-05 22:46:40 +0900

    Command 패턴
    처리하고 싶은 일을 메소드 호출이라는 동적인 처리방식이 아닌, 명령을 나타내는 클래스의 인스턴스(하나의 객체)로써 표현하는 패턴
    이를 통해 이력을 보존할 수도 있음

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,86 @@
+import command.Command;
+import command.MacroCommand;
+import drawer.DrawCanvas;
+import drawer.DrawCommand;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Main extends JFrame implements MouseMotionListener, WindowListener {
+    private MacroCommand history = new MacroCommand();
+    private DrawCanvas canvas = new DrawCanvas(400, 400, history);
+    private JButton clearButton = new JButton("clear");
+
+    public Main(String title) {
+        super(title);
+
+        this.addWindowListener(this);
+        this.addMouseMotionListener(this);
+        clearButton.addActionListener(e -> {history.clear(); canvas.repaint();});
+
+        Box buttonBox = new Box(BoxLayout.X_AXIS);
+        buttonBox.add(clearButton);
+        Box mainBox = new Box(BoxLayout.Y_AXIS);
+        mainBox.add(buttonBox);
+        mainBox.add(canvas);
+        getContentPane().add(mainBox);
+
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        Command cmd = new DrawCommand(canvas, e.getPoint());
+        history.append(cmd);
+        cmd.execute();
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
 
 
-public class Main {
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+
     public static void main(String[] args) {
-
+        new Main("Command Pattern Sample");
     }
 }
