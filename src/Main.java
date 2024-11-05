a21ddadeb867bde90546df31622f729d7baa7b05
splguyjr <splguyjr@naver.com>
2024-11-05 21:35:49 +0900

    Proxy 패턴
    대리인을 통해 일정 권한 이하의 작업들은 처리하고 꼭 필요한 경우에만 그 시점에 본인을 초기화하여 생성하고 처리

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-
+import proxy.Printable;
+import proxy.PrinterProxy;
 
 public class Main {
     public static void main(String[] args) {
-
+        Printable p = new PrinterProxy("Alice");
+        System.out.println("이름은 현재 " + p.getPrinterName() + "입니다.");
+        p.setPrinterName("Bob");
+        System.out.println("이름은 현재 " + p.getPrinterName() + "입니다.");
+        p.print("Hello, world.");
     }
 }
